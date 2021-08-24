c554c104bf0d9fc7649c46eb538e60f2e61dae14
gimsanghae <gobad820@gmail.com>
2021-08-24 22:48:50 +0900

    21-08-24

--- 0x0E/mergeSort.cpp
+++ 0x0E/mergeSort.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,m;
+int n = 5;
+int arr[5] = {5,4,3,2,1};
+
+void merge(int arr[],int l, int m, int r){
+	int tmp[n];
+}
+void mergeSort(int arr[], int l, int r){
+	if(l > r) // 한 칸이 남을 때 가지 분할
+		return;
+	int middle = (r+l)/2;
+	mergeSort(int arr[],l,middle);
+	mergeSort(int arr[],middle+1,r);
+	merge(int arr[],l,middle,r);
+}
 
 int main(int argc, const char *argv[]){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	cin >> n >> m;
-	for(int i = 0 ; i < n ; i++){}
+	
 	return 0;
 }
 
