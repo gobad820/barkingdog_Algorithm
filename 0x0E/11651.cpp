f86562944019bf78fcf52e82c400ae408288e9c6
gobad820 <57797741+gobad820@users.noreply.github.com>
2021-08-26 20:29:27 +0900

    Update 11651.cpp

--- 0x0E/11651.cpp
+++ 0x0E/11651.cpp
@@ -11,6 +11,49 @@ int n;
 vector<dots> v(100005);
 dots tmp[100005];
 
+void merge(int st, int en){
+	int mid = (st+en)/2;
+	int lidx = st; int ridx = mid;
+	for(int i = st ; i < en ; i++){
+		if(lidx == mid){
+			tmp[i].x = v[ridx].x;
+			tmp[i].y = v[ridx++].y;
+		}
+		else if(ridx == en){
+			tmp[i].x = v[lidx].x;
+			tmp[i].y = v[lidx++].y;
+		}
+		else if(v[lidx].y == v[ridx].y){
+			if(v[lidx].x <= v[ridx].x){
+				tmp[i].x = v[lidx].x;
+				tmp[i].y = v[lidx++].y;
+			}
+			else{
+				tmp[i].x = v[ridx].x;
+				tmp[i].y = v[ridx++].y;
+			}
+		}
+		else if(v[lidx].y < v[ridx].y){
+			tmp[i].x = v[lidx].x;
+			tmp[i].y = v[lidx++].y;
+		}
+		else{
+			tmp[i].x = v[ridx].x;
+			tmp[i].y = v[ridx++].y;
+		}
+	}
+	for(int i = st ; i < en ; i++){
+		v[i].x = tmp[i].x;
+		v[i].y = tmp[i].y;
+	}
+}
+void mergeSort(int st, int en){
+	if(st + 1 == en) return;
+	int mid = (st+en)/2;
+	mergeSort(st,mid);
+	mergeSort(mid,en);
+	merge(st,en);
+}
 
 int main(){
     ios::sync_with_stdio(0);
