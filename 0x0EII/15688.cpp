1f8dce9b6ded71c8a23952473f71ce48e34b4df2
gobad820 <57797741+gobad820@users.noreply.github.com>
2021-08-26 21:24:32 +0900

    Update 15688.cpp

--- 0x0EII/15688.cpp
+++ 0x0EII/15688.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int n;
-int numbers[1000005];
+int numbers[2000005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
     int num;
+    int maxnum = -1;
     for(int i = 0 ; i < n ; i++){
         cin >> num;
-        numbers[num]++;
+        numbers[num+1000000]++;
+        maxnum = max(maxnum,num);
     }
-    for(int i = 0 ; i < n ; i++){
+    for(int i = 0 ; i <= maxnum ; i++){
         if(numbers[i] == 0) continue;
         while(numbers[i] > 0){
             cout << i << endl;
