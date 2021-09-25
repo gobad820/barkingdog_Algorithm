a16b1a384d5eaf5dc32177725186049a01811398
gimsanghae <gobad820@gmail.com>
2021-09-25 20:09:43 +0900

    21-09-25

--- 0x0C/16987.cpp
+++ 0x0C/16987.cpp
@@ -5,33 +5,33 @@ int n;
 int egg[10][2];
 int cnt = 0;
 void func(int cur) {
-    if (cur == n) {
-        int tmp = 0;
-        for (int i = 0; i < n; i++)
-            if (egg[i][0] <= 0) tmp++;
-        cnt = max(cnt, tmp);
-        return;
+  if (cur == n) {
+    int tmp = 0;
+    for (int i = 0; i < n; i++)
+      if (egg[i][0] <= 0) tmp++;
+    cnt = max(cnt, tmp);
+    return;
+  }
+  for (int i = 0; i < n; i++) {
+    if (cur == i) continue;
+    int a = egg[cur][0];
+    int b = egg[i][0];
+    if (egg[cur][0] > 0 && egg[i][0] > 0) {
+      egg[cur][0] -= egg[i][1];
+      egg[i][0] -= egg[cur][1];
     }
-    for (int i = 0; i < n; i++) {
-        if (cur == i) continue;
-        int a = egg[cur][0];
-        int b = egg[i][0];
-        if (egg[cur][0] > 0 && egg[i][0] > 0) {
-            egg[cur][0] -= egg[i][1];
-            egg[i][0] -= egg[cur][1];
-        }
-        func(cur + 1);
-        egg[cur][0] = a;
-        egg[i][0] = b;
-    }
+    func(cur + 1);
+    egg[cur][0] = a;
+    egg[i][0] = b;
+  }
 }
 
 int main(int argc, char const *argv[]) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> egg[i][0] >> egg[i][1];
-    func(0);
-    cout << cnt;
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> egg[i][0] >> egg[i][1];
+  func(0);
+  cout << cnt;
+  return 0;
 }
