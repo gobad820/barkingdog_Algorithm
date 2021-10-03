8b0c7236d0066fbfa2682e968eb8e961fee09219
gimsanghae <gobad820@gmail.com>
2021-10-03 22:37:55 +0900

    21-10-03

--- 0x0D/hello.cpp
+++ 0x0D/hello.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main() {
-  string output;
-  cout << "Teste" << endl;
-  cin >> output;
-  cout << output << endl;
+  int a, b;
+  a = 10;
+  cout << a;
+  cin >> b;
+  cout << b;
   return 0;
 }
